9f6e95b40e8a8b61a8f8cfc95ead90cf7ff0a830
zhangchangming <719298118@qq.com>
2018-11-12 21:19:06 +0800

    2018 11 12

--- pages/repair/repair.js
+++ pages/repair/repair.js
@@ -1,19 +1,10 @@
-// pages/repair/repair.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
       awaitAll: 'optFor',
       awaitPayment: '',
       awaitService: '',
       awaitAppraise: '',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
@@ -53,52 +44,7 @@ Page({
             awaitAppraise: 'optFor'
         });
     },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    bindOrderDetail:function(){
+        console.log(1);
+    }
 })
